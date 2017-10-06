3a9202b54baaadf38d51797dd6dc9df80763b7f4
nc <charles.saternos@gmail.com>
2017-10-06 10:55:58 -0400

    fixed warnings with fun fun side effects

--- type_check/type.ml
+++ type_check/type.ml
@@ -1,14 +1,5 @@
 open MParser
 
-(* TODO: update this
-sexp ::= '(' definition | atom {sexp | atom}* ')'
-definition ::= 'def' symbol : {symbol | (symbol ...)}
-atom ::= symbol | string | number
-symbol ::= letter { letter | number }*
-number ::= digit+
-string ::= '"' character* '"'
-*)
-
 type ltype =
   | NumberT
   | BoolT
@@ -39,12 +30,29 @@ exception Type_mismatch of string * string
 (* util *)
 
 let rec drop h n =
-  if n == 0 then h else (drop (match h with a::b -> b) (n-1))
+  if n == 0 then h else
+    match h with
+    | [] -> []
+    | h::t -> drop t (n - 1)
 
 let rec take h n =
-  if n == 0 then [] else (match h with a::b -> a :: (take h (n - 1)))
+  if n == 0 then []
+  else
+    match h with
+    | [] -> []
+    | a::b -> a :: (take h (n - 1))
 
 (* parsing *)
+
+(*
+ident ::= letter { letter | number }*
+sexp ::= '(' definition | atom {sexp | atom}* ')'
+definition ::= 'def' symbol : {symbol | (symbol ...)}
+atom ::= symbol | string | number
+symbol ::= letter { letter | number }*
+number ::= digit+
+string ::= '"' character* '"'
+*)
 
 let ident = letter >>=
   (fun f -> many_chars (letter <|> digit) |>>
@@ -98,8 +106,8 @@ let sexp_to_string s =
                     | Symbol s -> "'" ^ s
                     | Number s -> string_of_int s in
   match s with
-    | Definition d -> "(" ^ d.name ^ " : " ^ type_to_string d.ty ^ ")"
-    | Application a -> "(" ^ a.fname ^ List.fold_left (fun s r -> s ^ " " ^ r) "" (List.map literal_to_string a.args) ^ ")"
+    | Definition {name; ty} -> "(" ^ name ^ " : " ^ type_to_string ty ^ ")"
+    | Application {fname; args} -> "(" ^ fname ^ List.fold_left (fun s r -> s ^ " " ^ r) "" (List.map literal_to_string args) ^ ")"
     | Primitive l -> literal_to_string l
 
 let parse (s: string) =
@@ -117,29 +125,29 @@ let check_prototype proto app tbl =
       | Symbol s -> Hashtbl.find tbl s in
   let assert_same_type = fun pe ae -> if (pe != (arg_type ae)) then raise (Type_mismatch (type_to_string pe, type_to_string (arg_type ae))) in
   try
-    List.map2 assert_same_type proto app
+    List.iter2 assert_same_type proto app
   with Invalid_argument _ -> raise (Wrong_arg_num (List.length app))
 
 let lookup_t tbl s = (try
   Hashtbl.find tbl s
     with Not_found -> raise (Unbound_variable s))
 
-let annotate_exprs e tbl =
-  let annotate_e e = match e with
-  | Application a -> check_prototype (match (lookup_t tbl a.fname) with (* drop the return value *)
+let check_exprs e tbl =
+  let check_e = function
+  | Application {fname; args} -> check_prototype (match (lookup_t tbl fname) with (* drop the return value *)
                                                 | FunctionT l -> take l ((List.length l) - 1)
-                                                | _ -> raise Wrong_type) a.args tbl; e
-  | Definition n -> Hashtbl.add tbl n.name n.ty; e
-  | Primitive _ -> e in
-  List.map annotate_e e
+                                                | _ -> raise Wrong_type) args tbl
+  | Definition {name; ty} -> Hashtbl.add tbl name ty
+  | Primitive _ -> () in
+  List.iter check_e e
 
 let parse_and_annotate (s: string) =
   match MParser.parse_string sexp_list s () with
-    | Success e -> annotate_exprs e (Hashtbl.create 32)
+    | Success e -> check_exprs e (Hashtbl.create 32); e
     | Failed (msg, e) ->
         failwith msg ;;
 
-let p e = List.map (fun s -> print_string s; print_newline()) e;;
+let p e = List.iter (fun s -> print_string s; print_newline()) e;;
 
 p(parse "(def x : number)");
 p(List.map sexp_to_string (parse_and_annotate "(def add : (number number number)) (def x : number) (add x 2) (add 1 2)"));
