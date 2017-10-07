80ed82d7d0ce0b1b46360323fb5fbc6bcd2c93e4
nc <charles.saternos@gmail.com>
2017-10-07 12:34:55 -0400

    added function types

--- type_check/type.ml
+++ type_check/type.ml
@@ -14,34 +14,35 @@ type literal =
   | True
   | Nil
 
-type ntpair = {name : string; ty : ltype}
-type alist = {fname : string; args : literal list}
+type binding = {name : string; ty : ltype}
+type proto = {name : string; args : binding list}
 
 type ntype =
-  | Definition of ntpair
-  | Application of alist
+  | Definition of binding
+  | Sexp of ntype list
   | Primitive of literal
 
-exception Unbound_variable of string
-exception Unknown_type of string
-exception Wrong_arg_num of int
-exception Wrong_type
-exception Type_mismatch of string * string
+exception Error of string
 
 (* util *)
 
-let rec drop h n =
+let last l = List.nth l ((List.length l) - 1)
+
+let rec drop n h =
   if n == 0 then h else
     match h with
     | [] -> []
-    | h::t -> drop t (n - 1)
+    | h::t -> drop (n - 1) t
 
-let rec take h n =
+let rec take n h =
   if n == 0 then []
   else
     match h with
     | [] -> []
-    | a::b -> a :: (take h (n - 1))
+    | h::t -> h :: (take (n - 1) t)
+
+let drop_last l = take ((List.length l) - 1) l
+
 
 (* parsing *)
 
@@ -73,7 +74,7 @@ let rec ptype =
                   | "number" -> NumberT
                   | "bool" -> BoolT
                   | "string" -> StringT
-                  | _ -> raise (Unknown_type t) in
+                  | _ -> raise (Error ("unknown type " ^ t)) in
     let ftype = (between (char '(') (char ')') (sep_by ident space)) |>>
       fun l -> FunctionT (List.map primtype l) in
       ftype <|> (ident |>> primtype)
@@ -82,16 +83,11 @@ let rec type_to_string t = match t with
                     | NumberT -> "number"
                     | BoolT -> "bool"
                     | StringT -> "string"
-                    | FunctionT atypes -> "(" ^ List.fold_left (fun s e -> s ^ " " ^ (type_to_string e)) "->" atypes ^ ")"
+                    | FunctionT atypes -> "(-> " ^ String.concat " " (List.map type_to_string atypes) ^ ")"
 
 let rec sexp e =
-  let ntype_to_literal l = match l with
-                            | Primitive l -> l
-                            | _ -> raise Wrong_type
-                            in
   let fcall = ident >>= fun f ->
-    (spaces >> (sep_by sexp space) |>> fun l ->
-      Application {fname=f; args=List.map ntype_to_literal l}) in
+    (spaces >> (sep_by sexp space) |>> fun l -> Sexp l) in
   let def = string "def" >> spaces >> ident >>= fun n ->
     (spaces >> char ':' >> spaces >> ptype |>> fun t ->
       Definition {name=n; ty=t}) in
@@ -99,7 +95,7 @@ let rec sexp e =
 
 let sexp_list = sep_by sexp space
 
-let sexp_to_string s =
+let rec sexp_to_string s =
   let literal_to_string e = match e with
                     | String s -> "\"" ^ s ^ "\""
                     | True -> "t"
@@ -108,7 +104,7 @@ let sexp_to_string s =
                     | Number s -> string_of_int s in
   match s with
     | Definition {name; ty} -> "(" ^ name ^ " : " ^ type_to_string ty ^ ")"
-    | Application {fname; args} -> "(" ^ fname ^ List.fold_left (fun s r -> s ^ " " ^ r) "" (List.map literal_to_string args) ^ ")"
+    | Sexp l -> "(" ^ List.fold_left (fun s r -> s ^ " " ^ r) "" (List.map sexp_to_string l) ^ ")"
     | Primitive l -> literal_to_string l
 
 let parse (s: string) =
@@ -120,51 +116,66 @@ let parse (s: string) =
 let lookup_t tbl s = (try
   match Hashtbl.find tbl s with
     | (ty, _) -> ty
-  with Not_found -> raise (Unbound_variable s))
+  with Not_found -> raise (Error ("unbound variable " ^ s)))
+
+let lookup_v tbl s = (try
+  match Hashtbl.find tbl s with
+    | (_, v) -> v
+  with Not_found -> raise (Error ("unbound variable " ^ s)))
 
 let check_prototype proto app tbl =
   let arg_type p = match p with
-      | String _ -> StringT
-      | Number _ -> NumberT
-      | True -> BoolT
-      | Nil -> BoolT
-      | Symbol s -> lookup_t tbl s in
-  let assert_same_type = fun pe ae -> if (pe != (arg_type ae)) then raise (Type_mismatch (type_to_string pe, type_to_string (arg_type ae))) in
+      | Primitive String _ -> StringT
+      | Primitive Number _ -> NumberT
+      | Primitive True -> BoolT
+      | Primitive Nil -> BoolT
+      | Primitive Symbol s -> lookup_t tbl s in
+  let assert_same_type = fun pe ae -> if (pe != (arg_type ae)) then
+    raise (Error ("expected " ^ (type_to_string pe) ^ " but got " ^ (type_to_string (arg_type ae)))) in
   try
     List.iter2 assert_same_type proto app
-  with Invalid_argument _ -> raise (Wrong_arg_num (List.length app))
+  with Invalid_argument _ -> raise (Error ("wrong number of arguments: " ^ (string_of_int (List.length app))))
 
 let context = global_context ()
-let the_module = create_module context "my jit"
+let the_module = create_module context "my comp"
 let builder = builder context
 let named_values:(string, (ltype * llvalue)) Hashtbl.t = Hashtbl.create 32
-let i32_type = i32_type context
-let i1_type = i1_type context
+
+let rec lty_to_llvmty = function
+  | NumberT -> i32_type context
+  | BoolT -> i1_type context
+  | StringT -> pointer_type (i8_type context)
+  | FunctionT l -> function_type (lty_to_llvmty (last l)) (Array.of_list (List.map lty_to_llvmty (drop_last l)))
 
 let rec emit tbl e =
   let emit_expr = function
-    | Number i -> const_int i32_type i
-    | True -> const_int i1_type 1
-    | Nil -> const_int i1_type 0
-    | Symbol s | String s -> const_string context s
-  in
-  let track_def = function
-    | Definition {name; ty} -> Hashtbl.add tbl name (ty, const_int i32_type 0)
-  in
+    | True -> const_int (lty_to_llvmty BoolT) 1
+    | Nil -> const_int (lty_to_llvmty BoolT) 0
+    | Number v -> const_int (lty_to_llvmty NumberT) v
+    | Symbol v | String v -> const_stringz context v in
+  let defvar name ty = Hashtbl.add tbl name (ty, const_int (lty_to_llvmty NumberT) 0) in
+  let emit_fundef name ty = declare_function name (lty_to_llvmty ty) the_module in
+  let funsym = function
+    | Primitive Symbol s -> s
+    | _ -> raise (Error "need function symbol to apply") in
   let emit_app = function
-    | Application {fname; args} -> check_prototype (match (lookup_t tbl fname) with (* drop the return value *)
-                                                    | FunctionT l -> take l ((List.length l) - 1)
-                                                    | _ -> raise Wrong_type) args tbl
-  in
+    | Sexp [] -> raise (Error "empty function application")
+    | Sexp (id :: args) -> check_prototype (match (lookup_t tbl (funsym id)) with (* drop the return value *)
+                                                    | FunctionT l -> take ((List.length l) - 1) l
+                                                    | _ -> raise (Error "wrong type")) args tbl in
   match e with
     | [] -> []
     | h :: t -> (match h with
       | Primitive p -> emit tbl t @ [emit_expr p]
-      | Definition _ as d -> track_def d; emit tbl t
-      | Application _ as a -> emit_app a; emit tbl t)
+      | Definition {name; ty = FunctionT _ as fty} -> emit tbl t @ [emit_fundef name fty]
+      | Definition {name; ty = _ as vty} -> defvar name vty; emit tbl t
+      | Sexp _ as a -> emit_app a; emit tbl t)
 
 let p e = List.iter (fun s -> print_string s; print_newline()) e;;
 
-p(List.map sexp_to_string (parse "(def x : number)"));
+(*p(List.map sexp_to_string (parse "(def x : number)"));*)
 
-List.iter dump_value (emit named_values (parse "(def x : number) 2 t nil symbol \"blah blah blah\""))
+let t = parse "(def super : (bool string number))" in
+let c = emit named_values t in
+Llvm_analysis.assert_valid_module the_module;
+List.iter dump_value c
