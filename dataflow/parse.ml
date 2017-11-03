9c5efa81f7bb3cdf38504667ced8fd7586284eed
nc <charles.saternos@gmail.com>
2017-11-03 16:40:19 -0400

    started working on dataflow language

---
+++ dataflow/parse.ml
@@ -0,0 +1,140 @@
+open MParser
+open Ppx_let
+
+type ltype =
+  | NumberT
+  | BoolT
+  | StringT
+  | FunctionT of proto
+and binding = {name : string; ty : ltype}
+and proto = {args : binding list; retty : ltype}
+
+
+type literal =
+  | Number of int
+  | Symbol of string
+  | String of string
+  | Stream of stream
+  | True
+  | Nil
+and stream = {hd : literal; cur : literal; next : (literal -> literal)}
+
+type ntype =
+  | Declaration of binding
+  | Definition of binding * ntype
+  | Sexp of ntype list
+  | Primitive of literal
+
+exception Error of string
+exception No_value
+
+(*
+ident ::= {symbol | letter} { symbol | letter | number }*
+string ::= '"' character* '"'
+symbol ::= ident
+number ::= digit+
+atom ::= symbol | string | number
+sexp ::= '(' definition | atom {sexp | atom}* ')'
+definition ::= 'def' symbol { '(' {'(' {symbol : symbol}+ ')'}* ')' }? : symbol sexp*
+*)
+
+let charsymbols = any_of "+-/*!%$?"
+
+let ident =
+  (letter <|> charsymbols) >>= fun f ->
+    many_chars (alphanum <|> charsymbols) |>> fun r ->
+      (Char.escaped f ^ r)
+
+let str = char '"' >> many_chars (none_of "\"") << char '"' |>> (fun s -> String s)
+let sym = ident |>> fun s -> match s with
+                              | "t" -> True
+                              | "nil" -> Nil
+                              | _ -> Symbol s
+
+let number =
+  many1_chars digit |>> fun n ->
+  Number (int_of_string n)
+let atom = sym <|> str <|> number
+
+let between_parens r = between (char '(') (char ')') r
+
+let rec ptype e =
+  let primtype = function
+                  | "number" -> NumberT
+                  | "bool" -> BoolT
+                  | "string" -> StringT
+                  | _ as t -> raise (Error ("unknown type " ^ t)) in
+  let parse_binding = ((between_parens
+  (ident >>= (fun na -> spaces >> ptype |>> fun ty -> {name=na; ty=ty}))) << spaces) in
+  let parse_proto = (between_parens (many (not_followed_by ident "" >> parse_binding) >>= fun args -> (ptype |>> fun ret -> {args = args; retty = ret}))) in
+  ((parse_proto |>> fun l -> FunctionT l) <|> (ident |>> primtype)) e
+
+let rec type_to_string = function
+                    | NumberT -> "number"
+                    | BoolT -> "bool"
+                    | StringT -> "string"
+                    | FunctionT proto -> "(-> " ^ String.concat " " (List.map (fun b -> "(" ^ b.name ^ " " ^ type_to_string b.ty ^ ")") proto.args) ^ " " ^ (type_to_string proto.retty) ^ ")"
+
+let rec sexp e =
+  let fcall = ident >>= fun f ->
+    (spaces >> (sep_by sexp space) |>> fun l -> Sexp ((Primitive (Symbol f)) :: l)) in
+  let name_type_pair = ident >>= fun n -> spaces >> char ':' >> spaces >> ptype |>> fun t -> {name=n; ty=t} in
+  let def = string "def" >> spaces >> ident >>= fun n ->
+    spaces >> (option (between_parens (sep_by (between_parens name_type_pair) spaces))) >>=
+    fun f ->
+      spaces >> char ':' >> spaces >> ptype >>= fun ty ->
+        let ty = match f with
+        | None -> ty
+        | Some v -> FunctionT {args=v; retty=ty} in
+        spaces >>? (((*many*) sexp |>> fun body -> (Definition ({name=n; ty}, body))) <|> return (Declaration {name=n; ty}))
+  in
+  ((atom |>> fun l -> Primitive l) <|> (between_parens (def <|> fcall))) e
+
+let sexp_list = sep_by sexp space
+
+let rec sexp_to_string s =
+  let rec literal_to_string = function
+                    | String s -> "\"" ^ s ^ "\""
+                    | True -> "t"
+                    | Nil -> "nil"
+                    | Symbol s -> "'" ^ s
+                    | Stream _ -> "<...>"
+                    | Number s -> string_of_int s in
+  match s with
+    | Declaration {name; ty} -> "(" ^ name ^ " : " ^ type_to_string ty ^ ")"
+    | Definition (def, body) -> let s = sexp_to_string (Declaration def) in
+                                String.sub s 0 ((String.length s) - 1) ^ " ...)"
+    | Sexp l -> "(" ^ (String.concat " " (List.map sexp_to_string l)) ^ ")"
+    | Primitive l -> literal_to_string l
+
+let parse (s: string) =
+  match MParser.parse_string sexp_list s () with
+    | Success e -> e
+    | Failed (msg, e) ->
+        failwith msg ;;
+
+let rec ieval = function
+  | Sexp l -> (iapply (ieval (List.hd l)) (List.map ieval (List.tl l)))
+  | Primitive l -> l
+  | Definition _ -> Nil
+  | Declaration _ -> Nil
+and iapply f args =
+  let apply_prim f args =
+    match f with
+  | Symbol "fby" ->
+      let upf = (List.nth args 2) in
+      Some {hd = List.hd args;
+      cur = List.hd args;
+      next = fun c -> (iapply upf [c])}
+  | _ -> None in
+  match apply_prim f args with
+  | None -> Nil
+  | Some s -> Stream s;;
+
+
+
+parse "(def x : bool)";;
+parse "(def x ((a : bool) (b : bool)) : number
+         (+ 1 a))";;
+
+ieval (List.hd (parse "(fby 1 1)"))
