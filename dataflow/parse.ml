e938e78a9b2d7159861868e308189b25a8a12198
ncharlie <charles.saternos@gmail.com>
2017-11-13 10:07:58 -0500

    got streams working mostly

--- dataflow/parse.ml
+++ dataflow/parse.ml
@@ -14,6 +14,7 @@ type literal =
   | String of string
   | Stream of stream
   | Lambda of lambda
+  | PLambda of (literal list -> literal)
   | True
   | Nil
 and stream = {hd : literal; cur : literal; next : (literal -> literal)}
@@ -98,9 +99,10 @@ let rec literal_to_string = function
   | Number i -> string_of_int i
   | Symbol s -> "'" ^ s
   | String s -> "\"" ^ s ^ "\""
-  | Stream s -> (literal_to_string s.hd)
+  | Stream s -> (literal_to_string s.cur)
   | True -> "t"
   | Lambda _ -> "<lambda>"
+  | PLambda _ -> "<plambda>"
   | Nil -> "nil" ;;
 
 let rec sexp_to_string s =
@@ -110,6 +112,15 @@ let rec sexp_to_string s =
                                 String.sub s 0 ((String.length s) - 1) ^ " ...)"
     | Sexp l -> "(" ^ (String.concat " " (List.map sexp_to_string l)) ^ ")"
     | Primitive l -> literal_to_string l
+
+let rec lookup env n =
+  match env with
+  | [] -> raise Not_found
+  | h :: t ->
+      try
+        Hashtbl.find h n
+      with _ ->
+        lookup t n ;;
 
 let parse (s: string) =
   match MParser.parse_string sexp_list s () with
@@ -132,49 +143,45 @@ let rec ieval env exp =
       | Primitive Symbol "quote" :: Primitive p :: _ -> p (* TODO: return a stream instead *)
       | Primitive Symbol "lambda" :: Sexp params :: body ->
           Lambda {params = (List.map (fun x -> x |> ntype_to_literal |> val_of_symbol) params); body; env}
-      | _ -> (iapply (ntype_to_literal (List.hd l)) (List.map (ieval env) (List.tl l))))
+      | _ -> (iapply (ieval env (List.hd l)) (List.map (ieval env) (List.tl l))))
   | Primitive Symbol s ->
-      (* lookup primitive here *)
-      Hashtbl.find (List.hd env) s
+      lookup env s
   | Primitive l -> l
   | Definition _ -> Nil
   | Declaration _ -> Nil
 and iapply f args =
-  let apply_prim f args =
-    match f with
-  | "fby" ->
-      let upf = (List.nth args 1) in
-      Some (Stream {hd = List.hd args;
-        cur = List.hd args;
-        next = fun c -> (iapply upf [c])})
-  | "first" ->
-      (match (List.hd args) with
-      | Stream s -> Some s.hd
-      | _ -> raise (Error "failed"))
-  | "next" ->
-      (match (List.hd args) with
-      | Stream s -> Some (Stream {hd = s.hd; cur = s.next s.cur; next = s.next})
-      | _ -> raise (Error "failed"))
-  | "add1" ->
-      (match (List.hd args) with
-      | Number n -> Some (Number (n + 1))
-      | _ -> raise (Error "failed"))
-  | _ -> None in
   match f with
-  | Symbol s ->
-      (match apply_prim s args with
-    | None -> Nil
-    | Some s -> s)
+  | PLambda f ->
+      (f args)
   | Lambda l ->
       let fenv = Hashtbl.create 32 in
       List.iter2 (fun a b -> Hashtbl.add fenv a b) l.params args;
       List.hd (List.rev (List.map (ieval (fenv :: l.env)) l.body))
   | _ -> raise (Error "cannot apply function") ;;
 
+let make_default_env =
+  let h = Hashtbl.create 32 in
+  Hashtbl.add h "add1" (PLambda (fun x -> match x with
+                        | [Number n] -> Number (n + 1)
+                        | _ -> raise (Error "can't add to non-number")));
+  Hashtbl.add h "fby" (PLambda (fun args ->
+    let upf = (List.nth args 1) in
+    (Stream {hd = List.hd args;
+        cur = List.hd args;
+        next = fun c -> (iapply upf [c])})));
+  Hashtbl.add h "first" (PLambda (fun args ->
+    (match (List.hd args) with
+      | Stream s -> s.hd
+      | _ -> raise (Error "failed"))));
+  Hashtbl.add h "next" (PLambda (fun args ->
+    (match (List.hd args) with
+      | Stream s -> (Stream {hd = s.hd; cur = s.next s.cur; next = s.next})
+      | _ -> raise (Error "failed"))));
+  [h];;
 
 parse "(def x : bool)";;
 parse "(def x ((a : bool) (b : bool)) : number
          (+ 1 a))";;
 
-print_endline (literal_to_string (ieval [Hashtbl.create 25] (List.hd (parse "(next (fby 1 add1))"))))
+print_endline (literal_to_string (ieval make_default_env (List.hd (parse "(next (next (fby 1 add1)))"))))
 (*print_endline (literal_to_string (ieval (Hashtbl.create 25) (List.hd (parse "(quote 2)"))))*)
