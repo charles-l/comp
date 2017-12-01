ecdc5a38c747b0d31d4bae9475817b78c91bd332
nc <charles.saternos@gmail.com>
2017-12-01 14:56:23 -0500

    rewrote so program is represented in directed graph

--- dataflow/df.rkt
+++ dataflow/df.rkt
@@ -1,120 +1,78 @@
 #lang racket
 (require racket/contract)
+(require sugar)
+(require graph)
 
-(struct func (params body env))
-(struct stream (first (current #:mutable) upf))
-(struct primfunc (f))
+(define (prompt-for-value name)
+  (string-append "Enter value for " (symbol->string name) ": ")
+  (read))
 
-(define/contract (lookup env n)
-  (-> (listof hash?) symbol? any)
-  (match env
-         ('() (error "No such binding " n))
-         ((cons h r)
-          (cond
-            ((hash-ref h n #f) => identity)
-            (else (lookup r n))))))
+(define *graph* (unweighted-graph/directed '()))
+(define-vertex-property *graph* node-f #:init (prompt-for-value $v))
 
-; evaluate list and return the result of the last eval
-(define (ieval-list li env)
-  (cond
-    ((null? li) 'nil)
-    ((cons? li)
-     (let ((r (ieval (car li) env)))
-       (if (null? (cdr li))
-         r
-         (ieval-list (cdr li) env))))))
+(define (internal-def? k)
+  (hash-has-key? *internal-funcs* k))
 
-(define (bindings->frame blist)
-  (make-hash blist))
+(define (add-uniq-vertex-with-f! e f)
+  (let ((n (gensym (string-append (->string e) "-"))))
+    (add-vertex! *graph* n)
+    (node-f-set! n f)
+    n))
 
-(define/contract (iapply f args)
-  (-> (or/c func? primfunc? stream?) list? any)
-  (cond
-    ((primfunc? f) (apply (primfunc-f f) args))
-    ((func? f)
-     (let ((fenv (bindings->frame (map cons (func-params f) args))))
-       (ieval-list (func-body f) (cons fenv (func-env f)))))
-    ((stream? f) (stream-current f))
-    (else
-      (error "expected a function, but got" f))))
-
-(define/contract (ieval expr env)
-  (-> (or/c stream? symbol? number? string? list?) (listof hash?) any)
-  (define (ieval-bindings li env)
-    (map
-      (match-lambda
-        ((cons b v)
-         (unless (symbol? b)
-           (error "expected symbol for binding: " b))
-         (cons b (ieval (car v) env)))
-        (else
-          (error "invalid binding"))) li))
+(define (compile-expr expr)
   (match expr
-         (`(quote ,e) e)
-         (`(let ,(list bindings ...) ,body ...)
-           ; desugar into a lambda
-           (let ((bindings (ieval-bindings bindings env)))
-             (iapply
-               (func (map car bindings) body env)
-               (map cdr bindings))))
-         (`(letrec ,(list bindings ...) ,body ...)
-           (let ((fr (bindings->frame (map cons (map car bindings) (make-list (length bindings) 'nil)))))
-             (for/list ((b bindings))
-                       (hash-set! fr (car b) (ieval (cadr b) (cons fr env))))
-             (ieval-list body (cons fr env))))
-         (`(lambda ,(list params ...) ,body ...)
-           (func params body env))
-         (`(.\ ,body ...)
-           (func '() body env))
-         ((list f args ...)
-          (iapply (ieval f env) (map ((curryr ieval) env) args)))
-         ((? number? string?) expr)
-         ((? symbol?) (lookup env expr))))
+    (`(define ,var ,expr)
+      (add-vertex! *graph* var)
+      ;TODO: node-f-set!
+      (add-directed-edge! *graph* (compile-expr expr) var)
+      var)
+    (`(fby ,f ,u)
+      (let* ((first (compile-expr f))
+             (upf (compile-expr u))
+             ; FIXME: this is wrong
+             (cur first)
+             (fby-node
+               (add-uniq-vertex-with-f! 'fby
+                                        ; FIXME: this is wrong too
+                                        (lambda ()
+                                          (set! cur (compile-expr upf))
+                                          ))))
+        (add-directed-edge! *graph* first fby-node)
+        (add-directed-edge! *graph* fby-node upf)
+        (add-directed-edge! *graph* upf fby-node)
+        fby-node
+        ))
+    ((list f args ...)
+     (let ((f (compile-expr f)))
+       (map (λ (e) (add-directed-edge! *graph* e f))
+            (map compile-expr args))
+       f))
+    ((? internal-def?)
+     (add-uniq-vertex-with-f! expr (hash-ref *internal-funcs* expr)))
+    ((? number? string?)
+     (add-vertex! *graph* expr)
+     (node-f-set! expr (lambda () expr))
+     expr)
+    ((? symbol?)
+     (cond
+       ((node-f expr #:default #f) => (lambda (_) expr))
+       (else (add-vertex! *graph* expr)
+             expr)))
+    (else (error "Unknown expression"))))
 
-(define (ensure! msg pred v)
-  (unless (pred v)
-    (error (string-append msg ":") v))
-  v)
+(define *internal-funcs*
+  (hash
+    '+ +
+    '- -
+    '* *
+    '/ /))
 
-(define (make-default-env)
-  (list
-    (hash
-      '+ (primfunc +)
-      '- (primfunc -)
-      '* (primfunc *)
-      '/ (primfunc /)
-      'fby (primfunc
-             (lambda (init upf)
-               (stream init init upf)))
-      'force (primfunc
-               (lambda (s)
-                 (ensure! "should be a stream" stream? s)
-                 (stream-current s)))
-      'first (primfunc
-               (lambda (s)
-                 (ensure! "should be a stream" stream? s)
-                 (stream-first s)))
-      'next! (primfunc
-               (lambda (s)
-                 (ensure! "should be a stream" stream? s)
-                 (set-stream-current! s (iapply (stream-upf s) '()))
-                 (stream-current s)))
-      'finite-stream (primfunc
-                       ; this - is just a little horrible.
-                       (lambda args
-                         (ensure! "need at least one element in stream" (compose not zero? length) args)
-                         (stream (car args) (car args)
-                                 (primfunc
-                                   (lambda ()
-                                     (cond
-                                       ((null? (cdr args)) 'nil)
-                                       (else
-                                         (set! args (cdr args))
-                                         (car args))))))))
-      )))
-
-(ieval '(letrec ((input (finite-stream 3 3 3 3 3))
-                 (sum (fby (first input) (.\ (+ (sum) (next! input)))))
-                 (n (fby 1 (.\ (+ (n) 1)))))
-          (/ (sum) (n)))
-       (make-default-env))
+;(compile-expr '(+ (- 1 2) 3))
+;(compile-expr '(define total (fby 0 (+ total x))))
+(compile-expr '(define n (fby 3 (+ n 2))))
+(compile-expr '(define prime (fby 2 (whenever n (isprime n)))))
+(let* ((graphviz-str (graphviz *graph*))
+       (escaped-graphviz (string-replace graphviz-str "\"" "\\\""))
+       (cmd (string-append "printf \"" escaped-graphviz "\" | dot -Tpng | feh -")))
+  (display escaped-graphviz)
+  (system cmd))
