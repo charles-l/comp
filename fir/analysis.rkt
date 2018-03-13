78b121d52b5ca7bbe12ed4630c2e307303f73c41
nc <charles.saternos@gmail.com>
2018-03-13 10:06:16 -0400

    added functions

--- fir/analysis.rkt
+++ fir/analysis.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (require nanopass/base)
+(require (only-in srfi/1 iota))
 
 (struct binding (name type))
 
@@ -49,23 +50,48 @@
                    ((if ,e0 ,e1 ,e2)
                     (unless (eq? (env-lookup-type env e0) 'bool)
                       (error "if condition must be of type bool"))
-                    `(if ,e0 ,e1 ,e2))))
+                    `(if ,(Expr e0 env) ,(Expr e1 env) ,(Expr e2 env)))))
 
 
 (define-parser parse-L0 L0)
 
 (define +word-size+ 4)
 
+(define *function-queue* '())
+
+(define (emit-function name body args)
+  (printf ".global ~a\n" name)
+  (printf ".type ~a @function\n" name)
+  (printf ".align 8\n")
+  (printf "~a:\n" name)
+  (printf "pushl %ebp\n")
+  (printf "movl %esp, %ebp\n")
+  (L1->x86 body (map
+                  (lambda (a i)
+                    (cons a (- i)))
+                  args
+                  (iota (length args) 2)))
+  (printf "movl %ebp, %esp\n")
+  (printf "popl %ebp\n")
+  (printf "ret\n"))
+
 (define (L1->x86 expr env)
-  (define (lookup e)
-    (index-of env e))
+  (define (frame-locals)
+    (filter (compose positive? cdr) env))
+
+  (define (slot-index e)
+    (cdr (findf (lambda (x) (eq? (car x) e)) env)))
+
+  (define imm? (disjoin symbol? number? boolean?))
+
+  (define (imm->x86 e)
+    (match e
+      ((? symbol?) (format "~a(%ebp)" (- (* +word-size+ (slot-index e)))))
+      ((? number?) (format "$~a" e))
+      ((? boolean?) (format "$~a" (if e 1 0)))))
+
   (match expr
-    ((? symbol?)
-     (printf "movl %eax, ~a(%ebp)\n" (- (* +word-size+ (add1 (lookup expr))))))
-    ((? number?)
-     (printf "movl $~a, %eax\n" expr))
-    ((? boolean?)
-     (printf "movl $~a, %eax\n" (if expr 1 0)))
+    ((? imm?) (printf "movl ~a, %eax\n" (imm->x86 expr)))
     (`(if ,imm ,then-expr ,else-expr)
       (let ((else-label (gensym 'else)) (end-label 'end))
         (L1->x86 imm env)
@@ -79,18 +105,32 @@
     (`(let ,n ,v ,body)
       (printf "subl $4, %esp\n") ; allocate space for new value
       (L1->x86 v env)
-      (let ((slot (length env)))
-        (printf "movl %eax, ~a(%ebp)\n" (- (* +word-size+ (add1 slot))))
-        (L1->x86 body (cons n env)))
-      )))
+      (let ((slot (add1 (length (frame-locals)))))
+        (printf "movl %eax, ~a(%ebp)\n" (- (* +word-size+ slot)))
+        (L1->x86 body (cons (cons n slot) env)))
+      )
+    (`(λ ,args ,body)
+      (let ((n (gensym 'lambda)))
+        (set! *function-queue* (append *function-queue* (list (list n body args))))
+        (printf "movl $~a, %eax\n" n)))
+    (`(,f ,args ...)
+      (L1->x86 f env)
+      (for-each (lambda (v)
+                  (printf "pushl ~a\n" (imm->x86 v)))
+                args)
+      (printf "call *%eax\n"))))
 
-(printf ".global fir_entry\n")
-(printf ".type fir_entry @function\n")
-(printf ".align 8\n")
-(printf "fir_entry:\n")
-(printf "pushl %ebp\n")
-(printf "movl %esp, %ebp\n")
-(L1->x86 (unparse-L1 (type-check-and-discard-type-info (parse-L0 '(let f bool #f (if f 2 3))))) '())
-(printf "movl %ebp, %esp\n")
-(printf "popl %ebp\n")
-(printf "ret\n")
+
+(emit-function
+  "fir_entry"
+  (unparse-L1
+    (type-check-and-discard-type-info
+      (parse-L0
+        '(let f (λ (x) x)
+           (let g bool #t
+             (if g
+               (f 2)
+               (f 3)))))))
+  '())
+
+(for-each (lambda (x) (emit-function (first x) (second x) (third x))) *function-queue*)
